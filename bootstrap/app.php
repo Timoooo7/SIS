967bd436699ecb96e6cbad12523049e214b35e38
Timoooo7 <timothyarella7@gmail.com>
2024-07-16 22:55:38 +0700

    Update app.php
    Version 1 (80%)

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Http\Middleware\EnsureMethodMatch;
-use App\Http\Middleware\EnsureRouteExist;
 use App\Http\Middleware\EnsureUserHasRole;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;

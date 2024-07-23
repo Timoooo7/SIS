b688bab4205e2b9830bc495a8f09c74bd4f0e70e
Timoooo7 <timothyarella7@gmail.com>
2024-07-23 12:15:57 +0700

    Caching and update web route

--- routes/web.php
+++ routes/web.php
@@ -21,25 +21,6 @@ Route::get('/', function () {
     return view('welcome');
 })->name('intro');
 
-// Verifying email
-Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
-    $request->fulfill();
-
-    return route('intro');
-})->middleware(['auth', 'signed'])->name('verification.verify');
-
-// Resending email verification
-Route::post('/email/verification-notification', function (Request $request) {
-    $request->user()->sendEmailVerificationNotification();
-
-    return back()->with('message', 'Verification link sent!');
-})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
-
-// Registered, but hasn't verify email
-Route::get('/email/verify', function () {
-    return view('auth.verify-email');
-})->middleware('auth')->name('verification.notice');
-
 // Google Authentication
 Route::get('/google/auth/callback', [GoogleController::class, 'callback']);
 

27a5e12160a72593f6c8fc869b6833c5a18f953b
Timoooo7 <timothyarella7@gmail.com>
2024-08-03 22:15:54 +0700

    add seeder for balance table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,63 +15,25 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         setlocale(LC_TIME, 'IND');
-        // User::factory(10)->create();
-        // DB::table('users')->insert(
-        //     [
-        //         [
-        //             'name' => 'Timothy Arella',
-        //             'email' => 'timothyarella7@gmail.com',
-        //             'phone' => '089626124115',
-        //             'password' => '$2y$12$17Fy1AkLcPTCq8qKtn8TdOPgecTSTI9c26wHe57YqQuPxtOWA4yU2',
-        //             'roles_id' => 1,
-        //             'email_verified_at' => now(),
-        //             'created_at' => now()
-        //         ],
-        //         [
-        //             'name' => 'User2',
-        //             'email' => 'user2@mail.com',
-        //             'phone' => '089626124116',
-        //             'password' => '$2y$12$17Fy1AkLcPTCq8qKtn8TdOPgecTSTI9c26wHe57YqQuPxtOWA4yU2',
-        //             'roles_id' => 2,
-        //             'email_verified_at' => now(),
-        //             'created_at' => now(),
-        //         ],
-        //         [
-        //             'name' => 'User3',
-        //             'email' => 'user3@mail.com',
-        //             'phone' => '089626124117',
-        //             'password' => '$2y$12$17Fy1AkLcPTCq8qKtn8TdOPgecTSTI9c26wHe57YqQuPxtOWA4yU2',
-        //             'roles_id' => 3,
-        //             'email_verified_at' => now(),
-        //             'created_at' => now(),
-        //         ],
-        //         [
-        //             'name' => 'User4',
-        //             'email' => 'user4@mail.com',
-        //             'phone' => '089626124118',
-        //             'password' => '$2y$12$17Fy1AkLcPTCq8qKtn8TdOPgecTSTI9c26wHe57YqQuPxtOWA4yU2',
-        //             'roles_id' => 4,
-        //             'email_verified_at' => now(),
-        //             'created_at' => now(),
-        //         ],
-        //         [
-        //             'name' => 'User5',
-        //             'email' => 'user5@mail.com',
-        //             'phone' => '089626124119',
-        //             'password' => '$2y$12$17Fy1AkLcPTCq8qKtn8TdOPgecTSTI9c26wHe57YqQuPxtOWA4yU2',
-        //             'roles_id' => 4,
-        //             'email_verified_at' => now(),
-        //             'created_at' => now(),
-        //         ],
-        //     ]
-        // );
 
+        // Seeds roles table
         DB::table('roles')->insert(
             [
                 ['name' => 'Chief Executive Officer'],
                 ['name' => 'Financial Officer'],
                 ['name' => 'Operational Officer'],
                 ['name' => 'Staff']
+            ]
+        );
+
+        // Seeds balance table
+        DB::table('roles')->insert(
+            [
+                [
+                    'expense' => 0,
+                    'income' => 0,
+                    'balance' => 0
+                ],
             ]
         );
     }
